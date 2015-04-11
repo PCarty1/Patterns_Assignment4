7554e96f83321b28bd371914427cb3f116f6e9ac
Paul Carty <paulracarty@gmail.com>
2015-04-11 19:44:03 +0100

    Basic Patterns Shopping App

---
+++ patternsShopping/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PatternsShopping::Application.config.secret_token = '3b4ea8fd87de3d677a4a9aa05cf4dc33357e1da04c7785b315b7fd4059405831b4fe81c4e43d99f4c58999c34ebc3f0a026f82f3b2d1cc99566ebf07200d1e9a'
